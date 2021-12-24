810f8c139af4c62e4598588231d27a1fbe74f0f2
郑传亮 <zhengchuanliang@qq.com>
2021-12-24 17:38:52 +0800

    新增京东登录页面

--- cl-myapp/src/main.ts
+++ cl-myapp/src/main.ts
@@ -4,6 +4,7 @@ import router from "./router"
 import utils from '@/ts/utils'
 import store from '@/store/index'
 import Components from '@components/index'
+import '@/css/style.css'
 
 const app = createApp(App)
 app.config.globalProperties.$utils = utils;
